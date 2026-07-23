47451013a1142aba1266be3c5d0c4b2db85f136c
Sidhdesh Jindal <150220323+Lord-Siddi@users.noreply.github.com>
2026-07-23 15:34:52 +0530

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string>st;
+        string curr="";
+
+        for(int i=0;i<=path.size();i++){
+            if(i==path.size()||path[i]=='/'){
+                if(curr==""||curr=="."){}
+                else if(curr==".."){
+                    if(!st.empty())st.pop();
+                }else{
+                    st.push(curr);
+                }
+                curr="";
+            }else{
+                curr+=path[i];
+            }
+        }
+        vector<string>dirs;
+
+        while(!st.empty()){
+            dirs.push_back(st.top());
+            st.pop();
+        }
+        reverse(dirs.begin(),dirs.end());
+        string res;
+        for(auto dir:dirs){
+            res+="/";
+            res+=dir;
+        }
+        return res.empty()?"/":res;
+    }
+};
