57c588e090fc32491fa9845cf61e8d8e88c593db
Sidhdesh Jindal <150220323+Lord-Siddi@users.noreply.github.com>
2026-08-02 16:53:35 +0530

    Add: verifying-an-alien-dictionary - submission-0

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        vector<int>rank(26);
+
+        for(int i=0;i<26;i++){
+            rank[order[i]-'a']=i;
+        }
+
+        for(int i=0;i<words.size()-1;i++){
+            string& a=words[i];
+            string& b=words[i+1];
+
+            int j=0;
+
+            while(j<a.size()&&j<b.size()&&a[j]==b[j]){
+                j++;
+            }
+            if(j==min(a.size(),b.size())){
+                if(a.size()>b.size())return false;
+            }else{
+                if(rank[a[j]-'a']>rank[b[j]-'a'])return false;
+            }
+
+
+        }
+return true;
+    }
+};
